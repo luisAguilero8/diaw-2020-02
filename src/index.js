e662f5962abfddd162dcd57fc3d2cddad0e04149
luisAguilero8 <panaluisaguilera@mail.com>
2021-01-22 01:42:35 -0600

    Materialize Funcionando

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-alert */
 /* eslint-disable no-console */
 // Importar los estilos
-import './styles/styles.css';
+// import './styles/styles.css';
+import 'materialize-css/dist/css/materialize.css';
+import 'materialize-css/dist/js/materialize';
 
 console.log('Modern JS Working !!! Thanks Babel ... ');
 // const show = (message = 'Saludo por defecto') => {
